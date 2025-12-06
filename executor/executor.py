2baff46c2d4bb4d045c54284dc1ced5af0ee6e83
Alexrmsu <alexmagnasura@gmail.com>
2025-12-06 04:15:28 -0300

    kafka broker on compose

--- executor/executor.py
+++ executor/executor.py
@@ -1,8 +1,21 @@
 import json
-import zendriver
 
 from typing import List
 
 def read_instructions(json_payload) -> None | List:
     get_instructions = json.loads(json_payload).get('instructions', None)
     return get_instructions
+
+
+
+if __name__ == "__main__":
+    instructive: list[dict] = [
+        {"hola": "mundo", "instructions": [
+            {"command": "print('Hello World!')"}
+        ]}
+    ]
+    final = []
+    for i in instructive:
+        x = read_instructions()
+        final.append(x)
+    print(final)
